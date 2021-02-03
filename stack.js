6528ff481f2b42690d5428ad59f3cf31fd0637d1
Isaac Householder <isaachouseholder@gmail.com>
2021-02-02 19:14:43 -0800

    practice

--- stack.js
+++ stack.js
@@ -7,7 +7,7 @@ class Stack {
   push(element) {
     this.items[this.count] = element;
     this.count++;
-    console.log(`${element} added to ${this.count}`);
+    // console.log(`${element} added to ${this.count}`);
     return this.count - 1;
   }
 
@@ -15,22 +15,22 @@ class Stack {
     if (this.count === 0) return undefined;
     let deleteItem = this.items[this.count - 1];
     this.count -= 1;
-    console.log(`${deleteItem} removed`);
+    // console.log(`${deleteItem} removed`);
     return deleteItem;
   }
 
   peek() {
-    console.log(`top element is ${this.items[this.count - 1]}`);
+    // console.log(`top element is ${this.items[this.count - 1]}`);
     return this.items[this.count - 1];
   }
 
   isEmpty() {
-    console.log(this.count == 0 ? "Stack is empty" : "Stack is not empty");
+    // console.log(this.count == 0 ? "Stack is empty" : "Stack is not empty");
     return this.count == 0;
   }
 
   size() {
-    console.log(`${this.count} elements in stack`);
+    // console.log(`${this.count} elements in stack`);
     return this.count;
   }
 
@@ -39,23 +39,32 @@ class Stack {
     for (let i = 0; i < this.count; i++) {
       string += this.items[i] + " ";
     }
-    return string
+    return string;
   }
+
+  clear() {
+    this.items = [];
+    this.count = 0;
+    // console.log('Stack cleared')
+    return this.items;
+  }
+
+  reverse() {}
 }
 
-const stack = new Stack();
+// const stack = new Stack();
 
-stack.isEmpty();
+// stack.isEmpty();
 
-stack.push(100);
-stack.push(200);
-stack.push(300);
+// stack.push(100);
+// stack.push(200);
+// stack.push(300);
 
-console.log(stack.print())
+// console.log(stack.print())
 
-stack.pop();
+// stack.pop();
 
-console.log(stack.print())
+// stack.clear()
+// console.log(stack.print())
 
-
-stack.peek();
+module.exports = Stack;
